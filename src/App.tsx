bf157b2a283c99e3bf66383eebdd007909d63ad6
EugeniaRodichkina <eugenia.emailmehere@gmail.com>
2022-12-03 17:27:21 +0700

    1:36

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import { CreateProduct } from './components/CreateProduct';
 import { ErrorMesage } from './components/ErrorMesage';
 import { Loader } from './components/Loader';
 import { Modal } from './components/Modal';
 import { Product } from './components/Product';
+import { ModalContext } from './context/ModalContext';
 import { useProducts } from './hooks/products';
 import { IProduct } from './models';
 
 function App() {
   const { products, loading, error, addProduct } = useProducts();
-  const [modal, setModal] = useState(false);
+  const { modal, open, close } = useContext(ModalContext);
 
   const createHandler = (product: IProduct) => {
-    setModal(false);
+    close();
     addProduct(product);
   };
 
@@ -24,14 +25,14 @@ function App() {
         <Product product={product} key={product.id} />
       ))}
       {modal && (
-        <Modal title="Create new Product" onClose={() => setModal(false)}>
+        <Modal title="Create new Product" onClose={close}>
           <CreateProduct onCreate={createHandler} />
         </Modal>
       )}
 
       <button
         className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-        onClick={() => setModal(true)}
+        onClick={open}
       >
         +
       </button>
