7cde3d7c46ec826854a0813a9c0e478cec76796f
EugeniaRodichkina <eugenia.emailmehere@gmail.com>
2022-12-03 17:50:09 +0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import { useContext } from 'react';
-import { CreateProduct } from './components/CreateProduct';
-import { ErrorMesage } from './components/ErrorMesage';
-import { Loader } from './components/Loader';
-import { Modal } from './components/Modal';
-import { Product } from './components/Product';
-import { ModalContext } from './context/ModalContext';
-import { useProducts } from './hooks/products';
-import { IProduct } from './models';
+import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
+import { AboutPage } from './pages/AboutPage';
+import { ProductsPage } from './pages/ProductsPage';
 
 function App() {
-  const { products, loading, error, addProduct } = useProducts();
-  const { modal, open, close } = useContext(ModalContext);
-
-  const createHandler = (product: IProduct) => {
-    close();
-    addProduct(product);
-  };
-
   return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMesage error={error} />}
-      {products.map((product) => (
-        <Product product={product} key={product.id} />
-      ))}
-      {modal && (
-        <Modal title="Create new Product" onClose={close}>
-          <CreateProduct onCreate={createHandler} />
-        </Modal>
-      )}
-
-      <button
-        className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-        onClick={open}
-      >
-        +
-      </button>
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+      dolorum blanditiis nemo praesentium facere ipsum quo mollitia quaerat
+      recusandae, accusamus velit eveniet, neque maxime incidunt nesciunt!
+      Voluptates sunt id ullam minima consequuntur minus, reiciendis nam nemo
+      tempore doloremque nihil, natus quam optio, ea voluptatibus ducimus
+      perferendis voluptas aperiam maxime provident.
+    </div>
+  );
+}
